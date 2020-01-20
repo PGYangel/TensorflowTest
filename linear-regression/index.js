4c9de218618032db44e38fff90ffddd0c2a5def0
yerong <241761025@qq.com>
2020-01-20 23:02:11 +0800

    no message

--- linear-regression/index.js
+++ linear-regression/index.js
@@ -23,14 +23,21 @@ window.onload = async () => {
         optimizer: tf.train.sgd(0.1)        //优化器：随机梯度下降法，0.1是学习速率，真实运用是试出来最优速率
     })
 
-    const inputs=tf.tensor(xs)
-    const labels=tf.tensor(ys)
-    await model.fit(inputs,labels,{
-        batchSize:4,
-        epochs:100,
-        callbacks:tfvis.show.fitCallbacks(
-            {name:'训练过程'},
+    const inputs = tf.tensor(xs)
+    const labels = tf.tensor(ys)
+    await model.fit(inputs, labels, {
+        batchSize: 4,//每次扔4个点数据
+        epochs: 30,//训练30次
+        callbacks: tfvis.show.fitCallbacks(
+            {name: '训练过程'},
             ['loss']
         )
     })
+
+    //训练完后执行预测5的值
+    const output = model.predict(tf.tensor([5]))//预测数据要是tensor数据
+    console.log(output.print());//预测结果也是tensor数据
+    //将tensor数据转换成一般js数据
+    console.log(output.dataSync());//输入是什么数据类型，转换后的数据类型也是一样的，所以是数组。
+    alert('预测x是5的时候，y的结果是：'+output.dataSync()[0]);
 };
